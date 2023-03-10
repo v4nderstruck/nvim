afa8334ade23b76d60d471b333e3727d9a255891
zensayyy <zensayyy@gmail.com>
2023-03-10 18:39:23 +0100

    init

---
+++ config.lua
@@ -0,0 +1,70 @@
+-- vim options
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.relativenumber = true
+
+-- general
+lvim.log.level = "info"
+lvim.format_on_save = {
+  enabled = true,
+  pattern = "*.lua",
+  timeout = 1000,
+}
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+
+lvim.colorscheme = "dracula"
+lvim.format_on_save = false
+
+-- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
+lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<leader>u"] = ":UndotreeToggle<cr>"
+
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+lvim.plugins = {
+  "tpope/vim-sleuth",
+  "mfussenegger/nvim-jdtls",
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          auto_trigger = true,
+          keymap = {
+            accept = "<M-l>",
+          }
+        }
+      })
+    end,
+  },
+  "dracula/vim",
+  "mbbill/undotree",
+}
+-- ignore jdtls config
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls" })
+-- set a formatter, this will override the language server formatting capabilities (if it exists)
+local sources = require "null-ls"
+sources.setup {
+  sources = {
+    sources.builtins.formatting.google_java_format,
+  },  
+}
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { 
+    
+    command = "google-java-format",
+    filetypes = { "java" } 
+  },
+}
+-- Automatically install missing parsers when entering buffer
+lvim.builtin.treesitter.auto_install = true
